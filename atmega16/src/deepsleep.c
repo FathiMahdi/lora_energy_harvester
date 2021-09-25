438be3791d39499a6f3c3ac84cec0029ee1871aa
FathiMahdi <electronics_forever@outlook.com>
2021-09-25 19:02:10 +0800

    adding atmega16

---
+++ atmega16/src/deepsleep.c
@@ -0,0 +1,19 @@
+#############################################
+## Makefile for avr ave/gcc dudde          ##
+## 25/Sep/2021                             ##
+## made by FATHI MAHDI (_*_*_*_)           ##
+#############################################
+#define F_CPU 1000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+
+
+int main()
+{
+
+
+
+
+
+    return 0;
+}
